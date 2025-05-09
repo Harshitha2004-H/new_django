2d71000f019ca5a210a7862b8f162d4bc7fc35eb
Harshitha2004-H <harshithahe5@gmail.com>
2025-05-09 11:46:18 +0530

    settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,5 @@
 pip install dj-database-url
+mport dj-database-url
+import os
+DATABASES = {
+ "default": dj_database_url.parse(os.environ.get("postgresql://test_db_0c7m_user:6ETFQYGC7vT6PBTggqsJG4BxuzjqHwqd@dpg-d0epges9c44c738a9bq0-a.oregon-postgres.render.com/test_db_0c7m"))
